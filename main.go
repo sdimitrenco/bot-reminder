70044ce976ce9b2c9f7dcd218aedfbddbd6e288c
stanislavdimitrenco <stanislav.dimitrenco@gmail.com>
2021-01-31 22:49:28 +0200

    change some

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/StanislavDimitrenco/bot-reminder/pkg/controlers"
 	"github.com/StanislavDimitrenco/bot-reminder/pkg/database"
 	"github.com/StanislavDimitrenco/bot-reminder/pkg/telegram"
@@ -39,4 +40,14 @@ func main() {
 	// Run listener
 	<-stopC
 
+}
+
+func terminationListening(terminateC chan os.Signal, stopC chan<- struct{}) {
+	<-terminateC
+
+	fmt.Println("Bot was stopped.")
+
+	// TODO: close db connections, sockets, net connections
+
+	stopC <- struct{}{}
 }
