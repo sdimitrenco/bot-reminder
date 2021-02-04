4b6b318f59daa4940a59c735f73dd0634255f84d
stanislavdimitrenco <stanislav.dimitrenco@gmail.com>
2021-02-04 16:29:08 +0200

    change db type on mysql

--- go.mod
+++ go.mod
@@ -11,6 +11,6 @@ require (
 	github.com/yanzay/tbot/v2 v2.2.0
 	golang.org/x/oauth2 v0.0.0-20210126194326-f9ce19ea3013
 	google.golang.org/api v0.37.0
-	gorm.io/driver/sqlite v1.1.4
+	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
 )
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,15 +1,27 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 //connect to database
 func Database() (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-	})
+	var host = os.Getenv("DATABASE_HOST")
+	var port = os.Getenv("DATABASE_PORT")
+	var user = os.Getenv("DATABASE_USER")
+	var password = os.Getenv("DATABASE_PASSWORD")
+	var dbname = os.Getenv("DATABASE_NAME")
+
+	//var dbInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslMode)
+	var dbInfo = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, port, dbname)
+
+	db, err := gorm.Open(mysql.Open(dbInfo), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
 	autoMigrate(db)
 
 	return db, err
